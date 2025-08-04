b418793daf229191f48f60ad1caddb5f9d515035
Hemanta119 <hrajbanshi119@gmail.com$>
2025-08-04 11:58:20 +0545

    Add project files.

---
+++ Expense Tracker/Controllers/Last.cs
@@ -0,0 +1,17 @@
+﻿//using Expense_Tracker.Models;
+
+//namespace Expense_Tracker.Controllers
+//{
+//    internal class Last<T>
+//    {
+//        internal int Where(Func<object, bool> value)
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public static implicit operator Last<T>(List<Transaction> v)
+//        {
+//            throw new NotImplementedException();
+//        }
+//    }
+//}
